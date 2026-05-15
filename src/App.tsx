ebe9a799b06d57a3e38baa3180aec64b58e020d8
pagkratisthanos <pagkratisthanos@gmail.com>
2026-05-15 19:39:18 +0300

    feat: add CounterWithCustomHook component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-// import FunctionalComponent from "./components/LessonTwo/FunctionalComponent.tsx";
+// impo
+// rt FunctionalComponent from "./components/LessonTwo/FunctionalComponent.tsx";
 // import ClassComponent from "./components/LessonTwo/ClassComponent.tsx";
 // import ArrowFunctionalComponent from "./components/LessonTwo/ArrowFunctionalComponent.tsx";
 // import ArrowFunctionalComponentWithProps from "./components/LessonThree/ArrowFunctionalComponentWithProps.tsx";
