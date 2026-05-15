73218ef3fb96bb508f3653cb29480e47201edd6a
pagkratisthanos <pagkratisthanos@gmail.com>
2026-05-15 18:51:10 +0300

    feat: add NameChanger component

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@
 // import ArrowFunctionalComponent from "./components/LessonTwo/ArrowFunctionalComponent.tsx";
 // import ArrowFunctionalComponentWithProps from "./components/LessonThree/ArrowFunctionalComponentWithProps.tsx";
 import Layout from "./components/Layout.tsx";
-import CounterAdvanced from "./components/LessonFour/CounterAdvanced.tsx";
+// import CounterAdvanced from "./components/LessonFour/CounterAdvanced.tsx";
+import NameChanger from "./components/LessonFive/NameChanger.tsx";
 // import Counter from "./components/LessonFour/Counter.tsx";
 // import ClassCounter from "./components/LessonFour/ClassCounter.tsx";
 // import {useState} from "react";
@@ -71,10 +72,14 @@ function App() {
             {/*</Layout>*/}
 
             {/*LESSON FOUR*/}
+            {/*<Layout addClasses="bg-gray-50">*/}
+            {/*    /!*<Counter/>*!/*/}
+            {/*    /!*<ClassCounter/>*!/*/}
+            {/*    <CounterAdvanced/>*/}
+            {/*</Layout>*/}
+
             <Layout addClasses="bg-gray-50">
-                {/*<Counter/>*/}
-                {/*<ClassCounter/>*/}
-                <CounterAdvanced/>
+                <NameChanger/>
             </Layout>
         </>
     )
